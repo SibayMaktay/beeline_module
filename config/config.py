c99d7d44e350746eb4baea73cf97c5c4b3499878
Nugumanov <nugumanovmuhammad@gmail.com>
2026-09-02 12:29:03 +0500

    0.0041

--- config/config.py
+++ config/config.py
@@ -41,7 +41,7 @@ utm5_turn_on_inet = int(os.getenv("UTM5_TURN_ON_INET", "1"))
 # ============================================================================
 module_host = os.getenv("MODULE_HOST", "127.0.0.1").strip()
 module_port = os.getenv("MODULE_PORT", "9090")
-module_api_key = os.getenv("MODULE_API_KEY", "bee_test").strip()
+module_api_key = os.getenv("MODULE_API_KEY", "bee_test")
 
 # ============================================================================
 # ЛОГИРОВАНИЕ
